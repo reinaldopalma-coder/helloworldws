e77592a8e3602288ff6b6dc6249b97d4d8b300ee
reinaldopalma-coder <reynaldo.palma@gmail.com>
2026-06-30 16:00:04 -0400

    Cambio en el timezone para que muestre bien la hora en chile

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,11 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@PostConstruct
+    public void init() {
+        // Forza a toda la aplicación de Java a usar la hora de tu país
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Santiago"));
+    }
 
 }
