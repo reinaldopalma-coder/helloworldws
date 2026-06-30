af193892d6df07f4e6cf07a70358900230ab8845
reinaldopalma-coder <reynaldo.palma@gmail.com>
2026-06-30 16:20:45 -0400

    Cambios para que se muestre en timezone chile PERO que no se guarde con ese timezone en la base de datos. Que se siga guardando en UTC

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
@@ -13,11 +9,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@PostConstruct
-    public void init() {
-        // Forza a toda la aplicación de Java a usar la hora de tu país
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Santiago"));
-    }
 
 }
